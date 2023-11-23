10c2bf149ef0d1afbb5c964a4f8347ac2c00f7b2
Kyrre Havik <kyrremann@gmail.com>
2023-11-23 14:58:57 +0100

    fix: fjernet unødvendige kommentarer

--- controllers/pod_controller.go
+++ controllers/pod_controller.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2023.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controllers
 
 import (
@@ -39,7 +23,6 @@ import (
 	networkingv1 "k8s.io/api/networking/v1"
 )
 
-// PodReconciler reconciles a Pod object
 type PodReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
@@ -69,24 +52,12 @@ const (
 //+kubebuilder:rbac:groups=networking.gke.io,resources=fqdnnetworkpolicies/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=networking.gke.io,resources=fqdnnetworkpolicies/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Pod object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.0/pkg/reconcile
 func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
 
 	var pod corev1.Pod
 	if err := r.Get(ctx, req.NamespacedName, &pod); err != nil {
 		if apierrors.IsNotFound(err) {
-			// we'll ignore not-found errors, since they can't be fixed by an immediate
-			// requeue (we'll need to wait for a new notification), and we can get them
-			// on deleted requests.
 			return ctrl.Result{}, nil
 		}
 		logger.Error(err, "unable to fetch Pod")
@@ -98,7 +69,6 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 	}
 
 	if err := r.defaultFQDNNetworkPolicyExists(ctx, pod.Namespace); err != nil {
-		logger.Info("Ignoring namespace as default fqdn netpol does not exist")
 		return ctrl.Result{}, nil
 	}
 
