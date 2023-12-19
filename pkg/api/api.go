9cc36ba95c4b84c7591bb0c0819f3094ee36602e
Kyrre Havik <kyrremann@gmail.com>
2023-12-19 14:00:55 +0100

    feat: json-logg for request-logger

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -3,30 +3,37 @@ package api
 import (
 	"context"
 	"log/slog"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/httplog/v2"
 	"github.com/navikt/knep/pkg/hostmap"
 )
 
-func New(ctx context.Context, incluster bool, onpremFirewallFilePath string, stats StatsSink, logger *slog.Logger) (*chi.Mux, error) {
+func New(ctx context.Context, incluster bool, onpremFirewallFilePath string, stats StatsSink, log *slog.Logger) (*chi.Mux, error) {
 	hostMap, err := hostmap.New(onpremFirewallFilePath)
 	if err != nil {
 		return nil, err
 	}
 
-	admissionHandler, err := NewAdmissionHandler(ctx, incluster, hostMap, stats, logger)
+	admissionHandler, err := NewAdmissionHandler(ctx, incluster, hostMap, stats, log)
 	if err != nil {
 		return nil, err
 	}
 
+	logger := httplog.NewLogger("api", httplog.Options{
+		JSON:             true,
+		Concise:          true,
+		RequestHeaders:   true,
+		MessageFieldName: "message",
+		TimeFieldFormat:  time.RFC850,
+	})
+
 	router := chi.NewRouter()
-	setupRoutes(router, admissionHandler)
-
-	return router, nil
-}
-
-func setupRoutes(router *chi.Mux, admissionHandler *AdmissionHandler) {
+	router.Use(httplog.RequestLogger(logger))
 	router.Use(middleware.Logger)
 	router.Post("/admission", admissionHandler.Validate)
+
+	return router, nil
 }
